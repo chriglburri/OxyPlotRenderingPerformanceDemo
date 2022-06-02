84d9637f675829adc2d52d977d753ed1d176cc89
Q3l <chriglburri@gmail.com>
2022-06-02 08:22:29 +0200

    add project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Linq;
+using System.Windows;
+
+namespace RenderingPerformance
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private const double HIGH_FREQUENCY = 3d;
+    private const double LOW_FREQUENCY = 1d / 500d;
+    private const string X_AXIS_KEY = nameof(X_AXIS_KEY);
+    private const string Y_AXIS_KEY = nameof(Y_AXIS_KEY);
+    private const string LEGEND_KEY = nameof(LEGEND_KEY);
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      Application.Current.MainWindow.WindowState = WindowState.Maximized;
+
+      Plot.Model = new PlotModel();
+
+      Plot.Model.Axes.Add(new OxyPlot.Axes.LinearAxis() { Title = "X-Axis", Position = OxyPlot.Axes.AxisPosition.Bottom, Key = X_AXIS_KEY });
+      Plot.Model.Axes.Add(new OxyPlot.Axes.LinearAxis() { Title = "Y-Axis", Position = OxyPlot.Axes.AxisPosition.Left, Key = Y_AXIS_KEY });
+      Plot.Model.Legends.Add(new OxyPlot.Legends.Legend() { LegendTitle = "Legend", Key = LEGEND_KEY });
+
+      // TODO: change the omega here for the 2 different test cases:
+      Plot.Model.Series.Add(BuildSeries(HIGH_FREQUENCY));
+      //Plot.Model.Series.Add(BuildSeries(LOW_FREQUENCY));
+
+      Plot.InvalidatePlot();
+    }
+
+    private Series BuildSeries(double omega)
+    {
+      int count = 20_000;
+      LineSeries series = new LineSeries
+      {
+        Title = $"{count} Points with ω={omega}",
+        XAxisKey = X_AXIS_KEY,
+        YAxisKey = Y_AXIS_KEY,
+        LegendKey = LEGEND_KEY
+      };
+      series.Points.AddRange(
+        Enumerable.Range(0, count)
+        .Select(t => new DataPoint(t / 100d, Math.Sin(t * omega)))
+        );
+      return series;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# OxyPlotRenderingPerformanceDemo
+OxyPlot Rendering Performance Demo
+===
+
 Demo for the rendering performance problem described in SO
+
+Reproduce the Problem with High Frequency Sine
+---
+
+* In MainWindow.xaml.cs make sure the line 33 is a comment:
+
+```csharp
+Plot.Model.Series.Add(BuildSeries(HIGH_FREQUENCY));
+//Plot.Model.Series.Add(BuildSeries(LOW_FREQUENCY));
+```
+
+* Run the program
+  * 20k HF sinus data points will be generated.
+  * The rendering takes some seconds.
+  * The plot area is almost covered with the signal.
+  * Every action like zooming or panning is noticeably slow.
+
+Prove That There Is No Problem Low Frequency Sine
+---
+
+* In MainWindow.xaml.cs make sure the line 32 is a comment:
+
+```csharp
+//Plot.Model.Series.Add(BuildSeries(HIGH_FREQUENCY));
+Plot.Model.Series.Add(BuildSeries(LOW_FREQUENCY));
+```
+
+* Run the program
+  * 20k LF sinus data points will be generated.
+  * The rendering takes less than a second.
+  * Most of the plot area is white. There is a sine line going from left to the right.
+  * Actions like zooming and panning are fast.
